16b94c98e77c646bb47b9c1839314462955bebfc
LouOz <louozor@gmail.com>
2023-09-22 14:24:40 -0500

    adding ec2

---
+++ environments/main.tf
@@ -0,0 +1,6 @@
+resource "aws_instance" "demo_instance" {
+  subnet_id              = "subnet-071e4be63d59a6296"
+  ami                    = "ami-03a6eaae9938c858c"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.sg-01916723f229e5911]
+}
