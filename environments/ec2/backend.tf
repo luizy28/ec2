aa47c0ffd57a421e7e32fa35ca01ce65ae4837d5
LouOz <louozor@gmail.com>
2023-09-22 15:45:46 -0500

    adding ec2

--- environments/ec2/backend.tf
+++ environments/ec2/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "bootcamp32-dev-13"
+    bucket = "bootcamp32-dev-13"
     region  = "us-east-1"
     key     = "ec2/terraform.tfstate"
     encrypt = true
