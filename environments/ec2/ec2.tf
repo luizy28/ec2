f18e8529859896f6b945c3c0a6f9121a531eeb73
LouOz <louozor@gmail.com>
2023-09-22 21:53:54 -0500

    test7

--- environments/ec2/ec2.tf
+++ environments/ec2/ec2.tf
@@ -2,5 +2,5 @@ resource "aws_instance" "demo_instance" {
   ami                    = "ami-03a6eaae9938c858c"
   instance_type          = "t2.micro"
   subnet_id              = data.aws_vpc.default-vpc
-  vpc_security_group_ids = [data.aws_security_group.sg-01916723f229e5911]
+  vpc_security_group_ids = [data.aws_security_group.launch-wizard-1]
 }
